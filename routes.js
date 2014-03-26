fbf3130169e7b188a705f85ae5d2e0772d49187d
ZHONGXINWEN <zhongxinwen@a-li.com.cn>
2014-03-26 16:41:26 +0800

    t

--- routes.js
+++ routes.js
@@ -97,8 +97,7 @@ exports = module.exports = function(app, passport) {
   app.get('/weixin/',require('./views/weixin/index').init);
   
   app.get('/weixin/relation/',require('./views/weixin/relation/index').relation_init);
-//  app.post('/weixin/relation/',require('./views/weixin/relation/index').relation_init);
-//  app.post('/weixin/relation/',require('./views/weixin/relation/index').relation_local);
+  app.post('/weixin/relation/',require('./views/weixin/relation/index').relation_local);
   
 //  app.get('/weixin/signup/',require('./views/weixin/signup/index').signup_init);
 //  app.post('/weixin/signup/',require('./views/weixin/signup/index').signup);
