137e7e6a7bb07ce47bc7f172d6caa282e2fab606
ZHONGXINWEN <zhongxinwen@a-li.com.cn>
2014-03-27 15:56:34 +0800

    test header user-agent

--- public/views/weixin/render.js
+++ public/views/weixin/render.js
@@ -20,7 +20,7 @@
 	  },
 	  
 	  render:function(){
-		  this.$el.html(this.template( this.model.attributes ));
+//		  this.$el.html(this.template( this.model.attributes ));
 		  var wx_url = $('#data-render').html();
 		  alert(wx_url)
 		  location.href =wx_url;
